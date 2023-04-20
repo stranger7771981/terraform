673dbd69c100a3cd0229f993b1a3a4f87037e25b
Uglov Denis <36145126+stranger7771981@users.noreply.github.com>
2023-04-20 20:52:12 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "ec2_ubuntu" {
     ami = "ami-007855ac798b5175e"
     instance_type = "t2.micro"
-
+    key_name = "ssh_connect"
     tags = {
     Name = "AWS Server"
     Owner = "Denis Uglov"
